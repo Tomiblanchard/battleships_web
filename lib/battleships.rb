aaea0d36f546c4cddea4fbcb50bc1a0f0475ab51
Tomiblanchard <tomiblanchard@gmail.com>
2015-03-31 11:31:26 +0100

    pass registering scenario

---
+++ lib/battleships.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+
+class Battleships < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/new_game' do
+    erb :new_game
+  end
+
+  set :views, Proc.new {File.join(root, "..", "views")}
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
